4686f4c84152b1ab7b77df8e79598d89a330e9ad
Ayiera-20 <ayieramyra@gmail.com>
2024-03-14 23:38:02 +0300

    Style: Enhance page layout and styling using Vuetify components

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -3,9 +3,41 @@
 </script>
 
 <template>
-    <div id = 'AboutUs'>
-    <h1>About Us</h1>
-
-    </div>
-
+  <v-container>
+    <v-layout row>
+      <v-flex xs12>
+        <v-card class="pa-3">
+          <v-card-title class="headline">About Us: University of Africa</v-card-title>
+          <v-card-text>
+            <p>
+              The University of Africa is a leading institution of higher learning dedicated to academic excellence, innovation, and community impact.
+              Established with a commitment to providing quality education, the university offers a diverse range of programs designed to empower students to achieve their academic and professional goals.
+            </p>
+            <p>
+              At the University of Africa, we believe in nurturing the next generation of leaders, thinkers, and innovators. Our faculty consists of accomplished scholars and industry experts who are passionate about teaching and mentoring students.
+              With state-of-the-art facilities and a dynamic learning environment, we provide students with the tools and resources they need to succeed in a rapidly changing world.
+            </p>
+            <p>
+              Our commitment to excellence extends beyond the classroom. We actively engage with the community through various outreach programs and initiatives, making a positive impact on society.
+              Through research and innovation, we strive to address the challenges facing our world and contribute to its sustainable development.
+            </p>
+            <p>
+              As a university rooted in values of integrity, diversity, and inclusivity, we welcome students from all backgrounds and cultures. Whether you are seeking to further your education or enhance your career prospects, the University of Africa is committed to helping you achieve your dreams and make a difference in the world.
+              Join us on a journey of discovery, growth, and success.
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  // Add any necessary data or methods
+};
+</script>
+
+<style>
+/* Add any custom styles */
+</style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,29 +3,50 @@
 </script>
 
 <template>
-    <div id= 'Login'>
-        <h1>Log in to UniApp</h1>
-        <div class="container">
-                <h1>Welcome Back</h1>
-                <label for="email"><b>Email</b></label>
-                <br>
-                <input type="text" placeholder="Enter Email" name="email" required>
-                <br><br>
-    
-                <label for="psw"><b>Password</b></label>
-                <br>
-                <input type="password" placeholder="Enter Password" name="psw" required>
-                <br><br>
-    
-                <label for="psw-repeat"><b>Repeat Password</b></label>
-                <br>
-                <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-                <button>Log in</button>
-                <p>Forgot your password?</p>
-                <p>Don't have an account?</p> 
-                <p><a href="">Sign Up</a></p>
-            </div>     
-    </div>
+  <div id="Login">
+    <v-container>
+      <v-layout column>
+        <v-card class="mx-auto" width="500">
+          <v-toolbar color="teal" dark>
+            <v-toolbar-title class="text-h6">
+              User Login
+            </v-toolbar-title>
+          </v-toolbar>
+          <v-container>
+            <v-text-field
+              v-model="email"
+              color="teal"
+              label="Email"
+              variant="underlined"
+            ></v-text-field>
 
-
+            <v-text-field
+              v-model="password"
+              color="teal"
+              label="Password"
+              variant="underlined"
+              type="password"
+            ></v-text-field>
+          </v-container>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="success">
+              Login
+              <v-icon right>mdi-chevron-right</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    email: null,
+    password: null,
+  }),
+};
+</script>
