21c0445ccb56e5a52d8c8804268b5d6de6d667f5
Ayiera-20 <ayieramyra@gmail.com>
2025-01-10 18:38:57 +0300

    style: adjust login and register buttons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,20 +3,14 @@
         <v-app-bar color="teal" >
             <v-app-bar-title>University of Africa</v-app-bar-title>
 
-            <v-spacer></v-spacer>
+            <!-- <v-spacer></v-spacer> -->
 
             <div>
                 <v-btn to ="/" color="white" dark> Home </v-btn>
             </div>
             <div>
                 <v-btn to="/about" color="white" dark>About </v-btn>
-            </div>
-            <div>
-                <v-btn to="/login" color="white" dark> Log In </v-btn>
-            </div>
-            <div>
-                <v-btn to="/Register" color="white" dark> Register </v-btn>
-            </div>            
+            </div>   
             <div>
                 <v-btn to="/students" color="white" dark> Students</v-btn>
             </div>
@@ -35,8 +29,21 @@
              <div>
                 <v-btn to="/Classroom" color="white" dark> Classroom </v-btn>
             </div>
+            <div class="login">
+                <v-btn to="/login" color="white" dark> Log In </v-btn>
+            </div>
+            <div>
+                <v-btn to="/Register" color="white" dark> Register </v-btn>
+            </div>         
             
             
         </v-app-bar>
     </div>
 </template>
+
+<script></script>
+<style>
+.login{
+    padding-left: 0px;
+}
+</style>
